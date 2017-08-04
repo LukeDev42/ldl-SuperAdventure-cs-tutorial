f87be3e411ace9cd9721586264285364a9972dcd
TheDoctor33 <ldlstarwars@gmail.com>
2017-08-03 23:10:21 -0400

    Add project files.

---
+++ SuperAdventure/SuperAdvetnure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdvetnure : Form
+    {
+        public SuperAdvetnure()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblExperience_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+    }
+}
